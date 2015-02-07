9d635dcb1087437e45bebafb3abc289840497ae1
Liam <liamfruzyna@gmail.com>
2015-02-06 18:20:26 -0600

    fixed reading

--- src/org/wildstang/sdlogreader/Deserialize.java
+++ src/org/wildstang/sdlogreader/Deserialize.java
@@ -1,11 +1,8 @@
 package org.wildstang.sdlogreader;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
@@ -15,31 +12,42 @@ import java.util.Map;
 
 public class Deserialize {
 	static List<List<Sensor>> stored;
-	static List<Map <String, Object>> e;
+	static List<Map<String, Object>> e = new ArrayList<Map<String,Object>>();
 	static boolean isDone = false;
-	
+
 	public static void deserial() throws IOException, ClassNotFoundException {
 		if (Main.logFile != null) {
 			File file = Main.logFile;
-			BufferedReader br = new BufferedReader(new FileReader(file));
-			String line;
-			while((line = br.readLine()) != null)
-			{
-			       ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(line.getBytes()));
-			       e.add((HashMap <String, Object>) in.readObject());
-			       in.close();
+			ObjectInputStream oin = new ObjectInputStream(new FileInputStream(file));
+			while (true) {
+				try {
+					e.add((HashMap<String, Object>) oin.readObject());
+				} catch (EOFException e) {
+					e.printStackTrace();
+					break;
+				} catch (ClassCastException e) {
+					e.printStackTrace();
+					continue;
+				}
 			}
-		    System.out.println("Deserialized");
-			stored = new ArrayList<List<Sensor>>();
+			oin.close();
 			for(int i = 0; i < e.size(); i++)
 			{
+				System.out.println("Map: " + i);
+				for (Map.Entry<String, Object> entry : e.get(i).entrySet())
+				{
+					System.out.println(entry.getKey() + " - " + entry.getValue());
+				}
+			}
+			System.out.println("Deserialized");
+			stored = new ArrayList<List<Sensor>>();
+			for (int i = 0; i < e.size(); i++) {
 				Map<String, Object> map = e.get(i);
 				List<Sensor> sensors = new ArrayList<>();
-				for (Map.Entry<String, Object> entry : map.entrySet())
-				{
-			    	sensors.add(new Sensor(entry.getKey(), entry.getValue() + ""));
-			    	System.out.println(stored.size());
-			    }
+				for (Map.Entry<String, Object> entry : map.entrySet()) {
+					sensors.add(new Sensor(entry.getKey(), "" + entry.getValue()));
+					System.out.println(stored.size());
+				}
 				stored.add(sensors);
 			}
 		}
--- src/org/wildstang/sdlogreader/LoggerVisualizationPanel.java
+++ src/org/wildstang/sdlogreader/LoggerVisualizationPanel.java
@@ -1,21 +1,25 @@
 package org.wildstang.sdlogreader;
 
+import java.util.List;
+
 import javax.swing.JPanel;
 
-
 public class LoggerVisualizationPanel extends JPanel {
-	
-	
+
 	public LoggerVisualizationPanel() {
 		if (Main.logFile != null) {
 			drawData();
 		}
 	}
-	
+
 	public void drawData() {
 		for (int i = 0; i < Deserialize.stored.size(); i++) {
-			add(Deserialize.stored.get(0).get(i));
+			List<Sensor> currentList = Deserialize.stored.get(i);
 			System.out.println(i);
+			for (int j = 0; j < currentList.size(); j++) {
+				add(currentList.get(j));
+				System.out.println(j);
+			}
 		}
 		Main.frame.pack();
 	}
