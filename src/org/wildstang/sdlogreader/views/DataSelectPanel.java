9d34599a36c63d0f35847b779402ca665ec343cb
Alex Guerra <aguerra7002@stu.d214.org>
2015-02-13 17:58:46 -0600

    Fixed green time thingy panel with resizing and offset issues

--- src/org/wildstang/sdlogreader/views/DataSelectPanel.java
+++ src/org/wildstang/sdlogreader/views/DataSelectPanel.java
@@ -10,14 +10,18 @@ import org.wildstang.sdlogreader.models.LogsModel;
 
 public class DataSelectPanel extends JPanel {
 
-	private static final String[] DATA_TYPES = { "", "Boolean", "Double", "String" };
+	private static final String[] DATA_TYPES = { "Select Type", "Boolean", "Double", "String" };
 
 	JComboBox<String> keySelected;
 	JComboBox<String> typeSelected;
-	String[] keys;
-
+	String[] keys = new String[1];
+	String[] tempKeys;
+	private static final String defaultKey = "Select key     ";
+	
+	
 	public DataSelectPanel(Color color) {
-		keySelected = new JComboBox<>();
+		keys[0] = defaultKey;
+		keySelected = new JComboBox<>(keys);
 		add(keySelected);
 		typeSelected = new JComboBox<>(DATA_TYPES);
 		add(typeSelected);
@@ -28,11 +32,11 @@ public class DataSelectPanel extends JPanel {
 	public void updateModel(LogsModel model) {
 
 		keys = model.getAllKeys();
-		String[] tempKeys = new String[keys.length + 1];
+		tempKeys = new String[keys.length + 1];
 		System.arraycopy(keys, 0, tempKeys, 1, keys.length);
-		tempKeys[0] = "Select a value";
+		tempKeys[0] = "SelectKey";
 		keys = tempKeys;
-
+		keys[0] = "Select Key";
 		keySelected.setModel(new DefaultComboBoxModel<>(keys));
 	}
 	public void clearAllFields() {
